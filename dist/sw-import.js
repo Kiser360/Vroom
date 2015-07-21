c4864f6ae668bb66b3b33c7ada50037cb04f725a
Kiser360 <dakrkb@mst.edu>
2015-07-20 22:37:05 -0500

    Experimentation with app styles

--- dist/sw-import.js
+++ dist/sw-import.js
@@ -7,12 +7,4 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-body {
-  background: #222224;
-  font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  color: #eee;
-}
-
-[drawer] {
-	background:#333;
-}
+importScripts('bower_components/platinum-sw/service-worker.js');
